f549a0eec4a94a3f66b60439994f36c32922c458
Sanila7 <donsanila@gmail.com>
2026-02-02 06:15:45 +0000

    Added to more files

--- cgi-bin/perl-get-echo.pl
+++ cgi-bin/perl-get-echo.pl
@@ -5,8 +5,17 @@ print "Content-type: text/html \n\n";
 # print HTML file top
 print <<END;
 <!DOCTYPE html>
-<html><head><title>GET Request Echo</title>
-</head><body><h1 align="center">Get Request Echo</h1>
+<html><head><!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P8836VK7');</script>
+<!-- End Google Tag Manager --><title>GET Request Echo</title>
+</head><body><!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8836VK7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) --><h1 align="center">Get Request Echo</h1>
 <hr>
 END
 
--- cgi-bin/perl-hello-html-world.pl
+++ cgi-bin/perl-hello-html-world.pl
@@ -6,10 +6,20 @@ print "Content-Type: text/html\n\n";
 print "<!DOCTYPE html>";
 print "<html>";
 print "<head>";
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P8836VK7');</script>
+<!-- End Google Tag Manager -->
 print "<title>Hello Sanila Silva</title>";
 print "</head>";
 print "<body>";
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8836VK7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 print "<h1 align=center>Hello Sanila Silva</h1><hr/>";
 print "<p>Hello Sanila Silva</p>";
 print "<p>This page was generated with the Perl programming langauge</p>";
